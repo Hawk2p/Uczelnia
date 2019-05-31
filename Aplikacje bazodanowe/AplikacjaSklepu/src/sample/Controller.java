9c2e8db935abd4a15343171c2de887cad594b368
Paweł <pawelekpols@gmail.com>
2019-05-31 16:09:42 +0200

    categories added

--- Aplikacje bazodanowe/AplikacjaSklepu/src/sample/Controller.java
+++ Aplikacje bazodanowe/AplikacjaSklepu/src/sample/Controller.java
@@ -1,31 +1,55 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.*;
+import javafx.scene.text.Text;
+
+import java.io.IOException;
+
 public class Controller {
-    public Controller(){}
+    @FXML
+    private Text amountOfItems;
+    @FXML
+    private Text money;
+    @FXML
+    private AnchorPane content;
+    @FXML
+    private Pane banner;
+    @FXML
+    private Pane categories;
 
-    public void wypisz(){
-        System.out.println("działa sobie coś");
+
+
+    public void mainSite(){
+        content.getChildren().clear();
+        content.getChildren().addAll(banner,categories);
+
+    }
+    public void clearBucket(){
+
+    }
+    public void categoryOne() throws IOException {
+        content.getChildren().clear();
+        Pane pane = FXMLLoader.load(getClass().getResource("firstCategory.fxml"));
+        content.getChildren().addAll(pane);
+
+    }
+    public void categoryTwo() throws IOException {
+        content.getChildren().clear();
+        Pane pane = FXMLLoader.load(getClass().getResource("secondCategory.fxml"));
+        content.getChildren().addAll(pane);
+    }
+    public void categoryThree(){
+        content.getChildren().clear();
+    }
+    public void textUs(){
+
+    }
+    public void printFacture(){
+
     }
 
-    public void wypiszone(){
-        System.out.println("działa sobie coś");
-    }
-
-    public void wypisztwo(){
-        System.out.println("działa sobie coś");
-    }
-
-    public void wypiszthree(){
-        System.out.println("działa sobie coś");
-    }
-
-    public void wypiszfour(){
-        System.out.println("działa sobie coś");
-    }
-
-    public void wypiszfive(){
-        System.out.println("działa sobie coś");
-    }
 
 
 
